1ff1ce6a547ce62d11a0877cdb7c6f4df117c35e
wsegbert65360 <wsegbert@gmail.com>
2026-05-23 21:34:28 -0500

    Use relative assets for iOS bundle

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "capacitor" ? "./" : "/",
   server: {
     host: "::",
     port: 8080,
